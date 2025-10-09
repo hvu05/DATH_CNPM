8e3b8ee1462b7fec86fc0da1f15a6f81569d4853
Hades1123 <dp1.1a7.si@gmail.com>
2025-10-09 18:39:15 +0700

    refactor and responsive

--- FE/src/pages/auth/login.tsx
+++ FE/src/pages/auth/login.tsx
@@ -1,23 +1,25 @@
+import { Link } from 'react-router'
 import './auth.scss'
 
 export const LoginPage = () => {
     return (
-        <>
-            <div className='login'>
-                <div className="login__title">
-                    Đăng nhập
-                </div>
-                <div className="login__input">
-                    <input type="text" className="login__phone" placeholder='Số điện thoại' />
-                    <input type="text" className="login__password"
-                        placeholder='Mật khẩu' />
-                </div>
-                <button className="login__button">Đăng nhập</button>
-                <div className="login__note">
-                    <span className="login__text">Bạn quên mật khẩu?</span>
-                    <span className='login__reset'>Đặt lại mật khẩu</span>
+        <div className="login-container">
+            <div className="login">
+                <h1 className="login__title">Đăng nhập</h1>
+                <form className="login__form">
+                    <input type="text" className="login__input" placeholder="Số điện thoại" />
+                    <input type="password" className="login__input" placeholder="Mật khẩu" />
+                    <button type="submit" className="login__button">
+                        Đăng nhập
+                    </button>
+                </form>
+                <div className="login__footer">
+                    <span className="login__footer-text">Bạn quên mật khẩu?</span>
+                    <Link to="/reset-password" /* Assuming this is your reset route */ className="login__footer-link">
+                        Đặt lại mật khẩu
+                    </Link>
                 </div>
             </div>
-        </>
+        </div>
     )
 }

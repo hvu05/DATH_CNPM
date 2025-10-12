b9da8d475bdac1860338cd2dc9d3bfbbbde39394
Hades1123 <dp1.1a7.si@gmail.com>
2025-10-12 10:18:45 +0700

    refactor css

--- FE/src/components/seller/layout/header.tsx
+++ FE/src/components/seller/layout/header.tsx
@@ -4,32 +4,32 @@ import menuIcon from '@/assets/menu-icon.svg'
 import searchIcon from '@/assets/search-icon.svg'
 import cartIcon from '@/assets/cart-icon.svg'
 import defaultAvatar from '@/assets/default-avatar-icon.svg'
-import { Link } from 'react-router'
+import { Link } from 'react-router-dom'
 
 export const Header = () => {
     return (
-        <header className="header">
-            <div className="header__left">
-                <Link to={'/'} className="header__home-link">
+        <header className="seller-header">
+            <div className="seller-header__left">
+                <Link to={'/'} className="seller-header__home-link">
                     <img src={homeIcon} alt="Home" />
                 </Link>
-                <button className="header__button header__button--category">
-                    <img src={menuIcon} alt="Menu" className="header__button-icon" />
-                    <span className="header__button-text">Danh mục</span>
+                <button className="seller-header__button seller-header__button--category">
+                    <img src={menuIcon} alt="Menu" className="seller-header__button-icon" />
+                    <span className="seller-header__button-text">Danh mục</span>
                 </button>
             </div>
 
-            <div className="header__search">
-                <input type="text" className="header__search-input" placeholder="Tìm kiếm sản phẩm..." />
-                <img src={searchIcon} alt="Search" className="header__search-icon" />
+            <div className="seller-header__search">
+                <input type="text" className="seller-header__search-input" placeholder="Tìm kiếm sản phẩm..." />
+                <img src={searchIcon} alt="Search" className="seller-header__search-icon" />
             </div>
 
-            <div className="header__right">
-                <button className="header__button header__button--cart">
-                    <img src={cartIcon} alt="Cart" className="header__button-icon" />
-                    <span className="header__button-text">Giỏ hàng</span>
+            <div className="seller-header__right">
+                <button className="seller-header__button seller-header__button--cart">
+                    <img src={cartIcon} alt="Cart" className="seller-header__button-icon" />
+                    <span className="seller-header__button-text">Giỏ hàng</span>
                 </button>
-                <img src={defaultAvatar} alt="Avatar" className="header__avatar" />
+                <img src={defaultAvatar} alt="Avatar" className="seller-header__avatar" />
             </div>
         </header>
     )
--- FE/src/pages/seller/profile/profile.tsx
+++ FE/src/pages/seller/profile/profile.tsx
@@ -3,35 +3,33 @@ import defaultAvatar from '@/assets/default-avatar-icon.svg'
 
 export const ProfilePage = () => {
     return (
-        <>
-            <div className='seller-profile-container'>
-                <h1>Thông tin cá nhân</h1>
-                <div className='seller-profile'>
-                    <div className='seller-profile__main'>
-                        <div className='avatar'>
-                            <img src={defaultAvatar} alt="avatar" />
-                        </div>
-                        <div className="name <div className='hr'></div>">
-                            <div className="label">Họ và tên</div>
-                            <div>Tên nhân viên</div>
-                        </div>
-                        <div className='hr'></div>
-                        <div className='phone'>
-                            <div className='label'>Số điện thoại</div>
-                            <div>0987249005</div>
-                        </div>
-                        <div className='hr'></div>
-                        <div className='gender'>
-                            <div className="label">Giới tính</div>
-                            <div>Nam</div>
-                        </div>
-                        <div className='hr'></div>
-                        <div className='button-container'>
-                            <button>Chỉnh sửa thông tin</button>
-                        </div>
+        <div className='seller-profile'>
+            <h1 className='seller-profile__title'>Thông tin cá nhân</h1>
+            <div className='seller-profile__card'>
+                <div className='seller-profile__main'>
+                    <div className='seller-profile__avatar-container'>
+                        <img className='seller-profile__avatar' src={defaultAvatar} alt="avatar" />
+                    </div>
+                    <div className="seller-profile__info-row">
+                        <div className="seller-profile__label">Họ và tên</div>
+                        <div className='seller-profile__value'>Tên nhân viên</div>
+                    </div>
+                    <div className='seller-profile__hr'></div>
+                    <div className='seller-profile__info-row'>
+                        <div className='seller-profile__label'>Số điện thoại</div>
+                        <div className='seller-profile__value'>0987249005</div>
+                    </div>
+                    <div className='seller-profile__hr'></div>
+                    <div className='seller-profile__info-row'>
+                        <div className="seller-profile__label">Giới tính</div>
+                        <div className='seller-profile__value'>Nam</div>
+                    </div>
+                    <div className='seller-profile__hr'></div>
+                    <div className='seller-profile__button-container'>
+                        <button className='seller-profile__button'>Chỉnh sửa thông tin</button>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }

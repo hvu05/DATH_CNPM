c6b62cc753fd8ba7483c0f1e24c3d2c10be1f713
Hades1123 <dp1.1a7.si@gmail.com>
2025-10-12 14:02:27 +0700

    optimize and fix edit profile page

--- FE/src/pages/seller/profile/edit.profile.tsx
+++ FE/src/pages/seller/profile/edit.profile.tsx
@@ -3,61 +3,63 @@ import defaultAvatar from '@/assets/seller/user.svg'
 
 export const EditProfilePage = () => {
     return (
-        <>
-            <div className="seller-edit-profile">
-                <h1 className='seller-edit-profile__title'>Thông tin cá nhân</h1>
-                <div className="seller-edit-profile__card">
-                    <div className="seller-edit-profile__main">
-                        <div className="seller-edit-profile__avatar-container">
-                            <img src={defaultAvatar} alt="avatar" className='seller-edit-profile__avatar' />
-                        </div>
-                        <div className="seller-edit-profile__row">
-                            <div className="seller-edit-profile__label">Họ và tên</div>
-                            <input className="seller-edit-profile__input"></input>
-                        </div>
-                        <div className="seller-edit-profile__row">
-                            <div className="seller-edit-profile__label">Số điện thoại</div>
-                            <input className="seller-edit-profile__input"></input>
-                        </div>
-                        <div className="seller-edit-profile__row seller-edit-profile__row-gender">
-                            <div className="seller-edit-profile__label">Giới tính</div>
-                            <div className='seller-edit-profile__input-gender-container'>
+        <div className="seller-edit-profile">
+            <h1 className='seller-edit-profile__title'>Thông tin cá nhân</h1>
+            <div className="seller-edit-profile__card">
+                <div className="seller-edit-profile__main">
+                    <div className="seller-edit-profile__avatar-container">
+                        <img src={defaultAvatar} alt="avatar" className='seller-edit-profile__avatar' />
+                    </div>
+                    <div className="seller-edit-profile__row">
+                        <label className="seller-edit-profile__label">Họ và tên</label>
+                        <input className="seller-edit-profile__input" />
+                    </div>
+                    <div className="seller-edit-profile__row">
+                        <label className="seller-edit-profile__label">Số điện thoại</label>
+                        <input className="seller-edit-profile__input disabled" disabled value={'09822222323'} />
+                    </div>
+                    <div className="seller-edit-profile__row">
+                        <label className="seller-edit-profile__label">Giới tính</label>
+                        <div className='seller-edit-profile__gender-options'>
+                            <div className='seller-edit-profile__gender-option'>
                                 <input
-                                    className="seller-edit-profile__input-male"
+                                    className="seller-edit-profile__radio custom-radio-indicator"
                                     type='radio'
-                                    id='seller-edit-profile__input-male'
+                                    id='gender-male'
                                     name='gender'
                                     value={'male'}
                                 />
-                                <label htmlFor="seller-edit-profile__input-male">Nam</label>
+                                <label htmlFor="gender-male">Nam</label>
+                            </div>
+                            <div className='seller-edit-profile__gender-option'>
                                 <input
-                                    className="seller-edit-profile__input-female"
+                                    className="seller-edit-profile__radio custom-radio-indicator"
                                     type='radio'
-                                    id='seller-edit-profile__input-female'
+                                    id='gender-female'
                                     name='gender'
                                     value={'female'}
                                 />
-                                <label htmlFor="seller-edit-profile__input-female">Nữ</label>
+                                <label htmlFor="gender-female">Nữ</label>
                             </div>
                         </div>
-                        <div className="seller-edit-profile__row">
-                            <div className="seller-edit-profile__label">Ngày sinh</div>
-                            <div className='seller-edit-profile__input-birth-container'>
-                                <input className="seller-edit-profile__input-day"></input>
-                                <input className="seller-edit-profile__input-month"></input>
-                                <input className="seller-edit-profile__input-year"></input>
-                            </div>
+                    </div>
+                    <div className="seller-edit-profile__row">
+                        <label className="seller-edit-profile__label">Ngày sinh</label>
+                        <div className='seller-edit-profile__birth-inputs'>
+                            <input className="seller-edit-profile__input" placeholder='Ngày' />
+                            <input className="seller-edit-profile__input" placeholder='Tháng' />
+                            <input className="seller-edit-profile__input" placeholder='Năm' />
                         </div>
-                        <div className="seller-edit-profile__row">
-                            <div className="seller-edit-profile__label">Email</div>
-                            <input className="seller-edit-profile__input"></input>
-                        </div>
-                        <div className="seller-edit-profile__button-container">
-                            <button className="seller-edit-profile__button">Cập nhật thông tin</button>
-                        </div>
+                    </div>
+                    <div className="seller-edit-profile__row">
+                        <label className="seller-edit-profile__label">Email</label>
+                        <input className="seller-edit-profile__input" type='email' />
+                    </div>
+                    <div className="seller-edit-profile__button-container">
+                        <button className="seller-edit-profile__button">Cập nhật thông tin</button>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }

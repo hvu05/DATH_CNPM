72c8c021dc1055873d2a7fe65701f4ed09e7b462
thoinguyenhuu <thoia6pc@gmail.com>
2025-10-17 20:35:26 +0700

    address

--- BE/src/middlewares/check-role.middleware.ts
+++ BE/src/middlewares/check-role.middleware.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { AppError, ErrorCode } from "../exeptions";
 
-export const checkRole = (requiredRole: string) => {
+export const checkRole = (requiredRoles: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    if(req.user?.role?.toUpperCase() !== requiredRole.toUpperCase()) 
-      throw new AppError(ErrorCode.FORBIDDEN, "Bạn không có quyền truy cập chức năng này");
-    next();
+    for(const requiredRole of requiredRoles){
+      if(req.user?.role?.toUpperCase() === requiredRole.toUpperCase()) return next();
+    }
+    throw new AppError(ErrorCode.FORBIDDEN, "Bạn không có quyền truy cập chức năng này");
   };
 };
 

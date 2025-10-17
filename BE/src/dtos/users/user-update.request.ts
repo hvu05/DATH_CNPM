a448eff1a716145bfc8d276343c54502a271660e
thoinguyenhuu <thoia6pc@gmail.com>
2025-10-17 20:34:14 +0700

    refactor code

---
+++ BE/src/dtos/users/user-update.request.ts
@@ -0,0 +1,9 @@
+import {z} from 'zod';
+
+export const UserUpdateSchema = z.object({
+  full_name: z.string().optional(),
+  password: z.string().min(6, "Password must be at least 6 characters").optional() ,
+  phone: z.string().length(10, "Phone number must be 10 digits").optional(),
+})
+
+export type UserUpdateRequest = z.infer<typeof UserUpdateSchema>;

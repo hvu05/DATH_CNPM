61aee2a0e09dc82ee94e06ad96c4b1474c166808
hvu05 <hoangvu06052005@gmail.com>
2025-11-25 20:00:04 +0700

    chỉnh sửa địa chỉ có thêm tên người nhận

--- FE/src/pages/client/order/index.tsx
+++ FE/src/pages/client/order/index.tsx
@@ -20,72 +20,83 @@ import { message } from 'antd';
 
 type OptionsPayment = 'COD' | 'VNPAY';
 
+
+
+//?================================================================================================
+//! LÚC GHÉP GIỎ HÀNG VÔ THÌ BẬT DÒNG (1) LÊN VÀ BỎ DÒNG (2). SAU ĐÓ BẬT CHỖ "TỔNG TIỀN" VÀ "CẦN THANH TOÁN" LÊN LÀ XONG
+//?================================================================================================
+
+
+
+
 export const OrderClient = () => {
     const [formChangeAddress, setFormChangeAddress] = useState<boolean>(false);
     const navigate = useNavigate();
     const location = useLocation();
-    const orderItems: OrderItem[] = location.state.order.order_items;
-    console.log('reere', location.state.order);
+    // const orderItems: OrderItem[] = location.state?.order?.order_items || null; //! (1)
+    // console.log('reere', location.state.order);
     const [statusPayment, setStatusPayment] = useState<OptionsPayment>('COD');
 
     const { data: profile, loading: loadingProfile } = useClientProfile();
 
     const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
 
-    // const orderItems: OrderItem[] = [
-    //     {
-    //         id: 1,
-    //         price_per_item: 25000000,
-    //         quantity: 2,
-    //         product_variant: {
-    //             id: 1,
-    //             product_id: 101,
-    //             color: "Đen",
-    //             storage: "128GB",
-    //             name: "iPhone 15 Pro",
-    //             price: 25000000
-    //         }
-    //     },
-    //     {
-    //         id: 2,
-    //         price_per_item: 28000000,
-    //         quantity: 1,
-    //         product_variant: {
-    //             id: 2,
-    //             product_id: 101,
-    //             color: "Trắng",
-    //             storage: "256GB",
-    //             name: "iPhone 15 Pro",
-    //             price: 28000000
-    //         }
-    //     },
-    //     {
-    //         id: 3,
-    //         price_per_item: 22000000,
-    //         quantity: 3,
-    //         product_variant: {
-    //             id: 3,
-    //             product_id: 102,
-    //             color: "Xanh",
-    //             storage: "512GB",
-    //             name: "Samsung Galaxy S24",
-    //             price: 22000000
-    //         }
-    //     },
-    //     {
-    //         id: 4,
-    //         price_per_item: 5000000,
-    //         quantity: 1,
-    //         product_variant: {
-    //             id: 4,
-    //             product_id: 103,
-    //             color: "Đỏ",
-    //             storage: "64GB",
-    //             name: "Xiaomi Redmi Note 13",
-    //             price: 5000000
-    //         }
-    //     }
-    // ];
+    //! (2)
+    const orderItems: OrderItem[] = [
+        {
+            id: 1,
+            price_per_item: 25000000,
+            quantity: 2,
+            product_variant: {
+                id: 1,
+                product_id: 101,
+                color: "Đen",
+                storage: "128GB",
+                name: "iPhone 15 Pro",
+                price: 25000000
+            }
+        },
+        {
+            id: 2,
+            price_per_item: 28000000,
+            quantity: 1,
+            product_variant: {
+                id: 2,
+                product_id: 101,
+                color: "Trắng",
+                storage: "256GB",
+                name: "iPhone 15 Pro",
+                price: 28000000
+            }
+        },
+        {
+            id: 3,
+            price_per_item: 22000000,
+            quantity: 3,
+            product_variant: {
+                id: 3,
+                product_id: 102,
+                color: "Xanh",
+                storage: "512GB",
+                name: "Samsung Galaxy S24",
+                price: 22000000
+            }
+        },
+        {
+            id: 4,
+            price_per_item: 5000000,
+            quantity: 1,
+            product_variant: {
+                id: 4,
+                product_id: 103,
+                color: "Đỏ",
+                storage: "64GB",
+                name: "Xiaomi Redmi Note 13",
+                price: 5000000
+            }
+        }
+    ];
+    
     const order_fake: OrderRequest = {
         province: selectedAddress?.province || 'Chưa chọn',
         ward: selectedAddress?.ward || 'Chưa chọn',
@@ -116,6 +127,7 @@ export const OrderClient = () => {
     };
 
     if (loadingProfile) return <p>Loading...</p>;
+    if(!orderItems) return <h1 style={{fontSize: '30px'}}>Mua lòng chọn sản phẩm trước khi vào trang này</h1>;
     return (
         <div className="client-order-detail">
             <div className="client-order-detail__main">
@@ -136,12 +148,12 @@ export const OrderClient = () => {
                                             </h3>
                                             <div className="client-order-detail__product-attrs">
                                                 <span>{item?.product_variant?.color}</span>
-                                                <span>{item.quantity}</span>
+                                                <span>{item?.quantity}</span>
                                             </div>
                                         </div>
                                     </div>
                                     <div className="client-order-detail__product-price">
-                                        {item.price_per_item.toLocaleString()} VNĐ
+                                        {item?.price_per_item.toLocaleString()} VNĐ
                                     </div>
                                 </div>
                                 {<hr className="client-order-detail__separator" />}
@@ -158,7 +170,7 @@ export const OrderClient = () => {
                             <p>
                                 <span className="client-order-detail__info-label">Họ và tên:</span>{' '}
                                 <span className="client-order-detail__info-value">
-                                    {profile?.full_name || 'Chưa xác định'}
+                                    {selectedAddress?.receive_name || profile?.full_name || 'Chưa xác định'}
                                 </span>
                             </p>
                             <p>
@@ -166,7 +178,7 @@ export const OrderClient = () => {
                                     Số điện thoại:
                                 </span>{' '}
                                 <span className="client-order-detail__info-value">
-                                    {profile?.phone || '0854747707'}
+                                    {selectedAddress?.phone || profile?.phone || '0854747707'}
                                 </span>
                             </p>
                         </div>
@@ -222,7 +234,7 @@ export const OrderClient = () => {
                     <div className="client-order-detail__summary-row">
                         <span>Tổng tiền</span>
                         <span className="client-order-detail__summary-price">
-                            {location.state.order.total.toLocaleString()} VNĐ
+                            {/* {location.state.order.total.toLocaleString()} VNĐ */}
                         </span>
                     </div>
                     <div className="client-order-detail__summary-row">
@@ -233,7 +245,7 @@ export const OrderClient = () => {
                     <div className="client-order-detail__summary-row client-order-detail__summary-row--final">
                         <span>Cần thanh toán</span>
                         <span className="client-order-detail__summary-price--final">
-                            {location.state.order.total.toLocaleString()} VND
+                            {/* {location.state.order.total.toLocaleString()} VND */}
                         </span>
                     </div>
                 </div>

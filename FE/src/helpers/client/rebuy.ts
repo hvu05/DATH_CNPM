0cab53a11a20ca23742efa1df9a39bec2b3c346e
hvu05 <hoangvu06052005@gmail.com>
2025-12-06 08:54:18 +0700

    fix total when rebuy

--- FE/src/helpers/client/rebuy.ts
+++ FE/src/helpers/client/rebuy.ts
@@ -4,10 +4,17 @@ import type { NavigateFunction } from 'react-router-dom';
 
 export const handleRebuy = (order: OrdersInOrder, navigate: NavigateFunction) => {
     const CartOrder = mapToCartItem(order)
+    const total: number = calculateTotal(CartOrder)
     // console.log('cartorder', CartOrder)
-    navigate(`/client/order`, { state: { orderItems: CartOrder } });
+    navigate(`/client/order`, { state: { orderItems: CartOrder, total: total} });
 };
-
+const calculateTotal = (CartOrder: CartItem[]): number => {
+    let total: number = 0;
+    CartOrder.forEach(item => {
+        total += item.price * item.quantity
+    })
+    return total
+}
 export const mapToCartItem = (order: OrdersInOrder): CartItem[] => {
     let cartItem: CartItem[] = [];
     order.order_items.forEach(item => {
